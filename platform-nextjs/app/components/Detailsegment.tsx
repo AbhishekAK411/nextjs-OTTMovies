5a45b181ee42b82a6313b7d42ede28eabd402790
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-01-17 18:42:36 +0530

    updates

--- platform-nextjs/app/components/Detailsegment.tsx
+++ platform-nextjs/app/components/Detailsegment.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import Link from "next/link";
+
 type DetailSegmentProps = {
     movieProps: Singlemovie
 }
 
-const Detailsegment: React.FC<DetailSegmentProps> = ({movieProps}: DetailSegmentProps) => {
-
-    console.log(movieProps);
+const Detailsegment: React.FC<DetailSegmentProps> = ({movieProps}: DetailSegmentProps) => {   
+    
     return (
         <>
             <div className="w-full h-full px-2 flex flex-col gap-2">
@@ -22,10 +23,12 @@ const Detailsegment: React.FC<DetailSegmentProps> = ({movieProps}: DetailSegment
                 </div>
                 <p className="pr-32 text-2xl text-justify tracking-tight">{movieProps.overview}</p>
                 <div className="flex w-full gap-5 pt-5">
-                    <button className="shadow-xl p-5 text-3xl rounded-2xl flex gap-2 items-center justify-center bg-[#128dd3] text-white font-medium uppercase">
-                        Watch now
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#dedede" viewBox="0 0 256 256"><path d="M240,128a15.74,15.74,0,0,1-7.6,13.51L88.32,229.65a16,16,0,0,1-16.2.3A15.86,15.86,0,0,1,64,216.13V39.87a15.86,15.86,0,0,1,8.12-13.82,16,16,0,0,1,16.2.3L232.4,114.49A15.74,15.74,0,0,1,240,128Z"></path></svg>
-                    </button>
+                    <Link href={movieProps?.homepage}>
+                        <button className="shadow-xl p-5 text-3xl rounded-2xl flex gap-2 items-center justify-center bg-[#128dd3] text-white font-medium uppercase">
+                            Watch now
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#dedede" viewBox="0 0 256 256"><path d="M240,128a15.74,15.74,0,0,1-7.6,13.51L88.32,229.65a16,16,0,0,1-16.2.3A15.86,15.86,0,0,1,64,216.13V39.87a15.86,15.86,0,0,1,8.12-13.82,16,16,0,0,1,16.2.3L232.4,114.49A15.74,15.74,0,0,1,240,128Z"></path></svg>
+                        </button>
+                    </Link>
                     <button className="shadow-xl p-5 text-3xl rounded-2xl flex gap-2 items-center justify-center bg-slate-700/10 text-slate-800 font-medium uppercase">
                         Add to watchlist
                     </button>
